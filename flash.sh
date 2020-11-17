0f370cdf45ea141623e618ed21a340e43e0d4be4
Karen <karenl.harper88@gmail.com>
2020-11-17 15:27:37 -0800

    Added bash script to aid in arduboy flashing

---
+++ flash.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#This script assumes the crate has already been built successfully. 
+
+cd ./target/avr-atmega32u4/debug/
+
+avr-objcopy -S -j .text -j .data -O ihex ardurust.elf ardurust.hex
+
+echo Press the reset button on the Arduboy now. 
+
+sleep 1
+
+avrdude -q -q -patmega32u4 -cavr109 -P/dev/ttyACM0 -b57600 -D -v -Uflash:w:ardurust.hex:i
+
+cd ../../../../
